7f4521d913339851fcb9acf9d22cf03ec37f2998
Balaji <mynameisbalag@gmail.com>
2020-10-28 11:18:43 +0530

    App indexing finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
+import intro from './screens/intro/intro.screen'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/intro" component={intro} />
+          <Route  path="/" />
+          <Redirect to='/' />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
