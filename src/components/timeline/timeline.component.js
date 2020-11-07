ca45bdebafce5ed7b139952a27e9dc48de1019ee
sv-mukunthan <43805907+sv-mukunthan@users.noreply.github.com>
2020-11-07 12:28:18 +0530

    image file names changed

--- src/components/timeline/timeline.component.js
+++ src/components/timeline/timeline.component.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './timeline.component.scss';
-import ThedayofSurgery from '../../assets/images/The  Day of Surgery/Group 778.svg';
+import ThedayofSurgery from '../../assets/images/The_Day_of_Surgery/Group_778.svg';
+import ReactScrollWheelHandler from "../../../node_modules/react-scroll-wheel-handler";
 
 const sample = [
     {value: 1, sub: "Test subject"},
@@ -12,13 +13,14 @@ const sample = [
 ]
 
 const Timeline = (props) => {
-    const myRef = React.createRef();
 
-    const onScroll = () => {
-        const scrollY = window.scrollY //Don't get confused by what's scrolling - It's not the window
-        const scrollTop = this.myRef.current.scrollTop
-        console.log(`onScroll, window.scrollY: ${scrollY} myRef.scrollTop: ${scrollTop}`)
-    }
+    const nextIndex = () => {
+        console.log("next");
+    };
+     
+    const prevIndex = () => {
+        console.log("previous");
+    };
 
 
     return (
@@ -37,14 +39,23 @@ const Timeline = (props) => {
                     </div>
                 </div>
                 <div className="sub_content_container">
-                    <div className="sub_content" onScroll={() => onScroll()}>
-                        <div className="sub_head">
-                            <div className="head_text">The Day of Surgery</div>
-                            <div className="head_desciption">We will ask you to use a special shower get that cleans your skin and removes germs.</div>
-                        </div>
-                        <div className="img_container">
-                            <img alt="day_of_img" src={ThedayofSurgery}></img>
-                        </div>
+                    <div className="sub_content">
+                        <ReactScrollWheelHandler
+                            upHandler={prevIndex}
+                            downHandler={nextIndex}
+                            style={{
+                                width: "100%",
+                                height: "100vh",
+                            }}
+                            >
+                            <div className="sub_head">
+                                <div className="head_text">The Day of Surgery</div>
+                                <div className="head_desciption">We will ask you to use a special shower get that cleans your skin and removes germs.</div>
+                            </div>
+                            <div className="img_container">
+                                <img alt="day_of_img" src={ThedayofSurgery}></img>
+                            </div>
+                        </ReactScrollWheelHandler>
                     </div>
                 </div>
             </div>
