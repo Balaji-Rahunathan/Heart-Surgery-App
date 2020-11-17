11088f6f847eb0205ed18f925093e4d0459a7e4c
sv-mukunthan <43805907+sv-mukunthan@users.noreply.github.com>
2020-11-17 12:36:11 +0530

    slider completed

--- src/components/timeline/timeline.component.js
+++ src/components/timeline/timeline.component.js
@@ -6,67 +6,37 @@ import ThedayofSurgery from '../../assets/images/The_Day_of_Surgery/Group_778.sv
 import ReactScrollWheelHandler from "../../../node_modules/react-scroll-wheel-handler";
 import Carousel from "react-elastic-carousel";
 
-const sample = [
-    {value: 1, sub: "Test subject"},
-    {value: 2, sub: "Test subject"},
-    {value: 3, sub: "Test subject"},
-    {value: 4, sub: "Test subject"},
-    {value: 5, sub: "Test subject"}
-]
-
 const Timeline = (props) => {
-    const [state, setState] = useSetState({currentIndex: 0, contentIndex: 0})
-    const [slideClass, setClass] = useState("topic_content_right");
+    const [state, setState] = useSetState({currentIndex: 0, currentContentIndex: 0})
     const slider = useRef(null);
 
+    const changeIndex = (direction) => {
+        console.log("sliderr", slider.current);
+        if(direction) {
+            if((props.data.data.length - 1 == state.currentIndex) && (props.data.data[state.currentIndex].content.length - 1 == state.currentContentIndex)) {
+                setState({currentIndex: 0});
+                slider.current.goTo(0);
+            } else {
+                if(props.data.data[state.currentIndex].content.length - 1 == state.currentContentIndex) {
+                    setState({currentIndex: state.currentIndex + 1})
+                    slider.current.goTo(0);
+                } else {
+                    slider.current.slideNext();
+                }
+            }
+        } else {
+            if(state.currentIndex == 0 && state.currentContentIndex == 0) {
 
-    const nextIndex = () => {
-        if(props.data.data.length - 1 == state.currentIndex) {
-            if(props.data.data[state.currentIndex].content.length - 1 == state.contentIndex) {
-                setState({contentIndex: state.contentIndex, currentIndex: state.currentIndex})
             } else {
-                setClass("topic_content_right");
-                setState({contentIndex: state.contentIndex + 1})
-                slider.current.slideNext();
-            }
-        } else {
-            setClass("topic_content_right");
-            if(props.data.data[state.currentIndex].content.length - 1 === state.contentIndex) {
-                let count = state.currentIndex + 1
-                setState({contentIndex: 0, currentIndex: count})
-            } else {
-                let count = state.contentIndex + 1
-                setState({contentIndex: count})
-                slider.current.slideNext();
+                if(state.currentContentIndex == 0) {
+                    setState({currentIndex: state.currentIndex - 1})
+                    slider.current.goTo(props.data.data[state.currentIndex].content.length - 1);
+                } else {
+                    slider.current.slidePrev();
+                }
             }
         }
-    };
-     
-    const prevIndex = () => {
-        // let rightSlide = document.getElementById('topic');
-        // rightSlide.classList.add('topic_content_left');
-        // rightSlide.classList.remove('topic_content_right');
-        if(state.currentIndex == 0) {
-            if(state.contentIndex !== 0) {
-                setClass("topic_content_left");
-                setState({contentIndex: state.contentIndex - 1});
-                slider.current.slidePrev();
-            } else {
-                setState({contentIndex: 0});
-            }
-        } else {
-            setClass("topic_content_left");
-            if(props.data.data[state.currentIndex].content.length - 1 == state.contentIndex) {
-                setState({contentIndex: state.contentIndex - 1})
-                slider.current.slidePrev();
-            } else if(state.contentIndex == 0 && state.currentIndex !== 0) {
-                setState({contentIndex: props.data.data[state.currentIndex - 1].content.length - 1, currentIndex: state.currentIndex - 1})
-            } else {
-                setState({contentIndex:  state.contentIndex - 1})
-                slider.current.slidePrev();
-            }
-        }
-    };
+    }
 
     const topicCheck = (index) => {
         setState({contentIndex: 0, currentIndex: index});
@@ -77,7 +47,6 @@ const Timeline = (props) => {
     return (
         <div className="time_line_container">
             <div className="time_line_content">
-                {console.log(console.log(state))}
                 <div className="sub_topic_container">
                     <div className="sub_topic_content">
                         <div className="topics">
@@ -93,34 +62,26 @@ const Timeline = (props) => {
                 <div className="sub_content_container">
                     <div className="sub_content">
                         <ReactScrollWheelHandler
-                            upHandler={prevIndex}
-                            downHandler={nextIndex}
+                            upHandler={()=> changeIndex(false)}
+                            downHandler={() =>changeIndex(true)}
                             style={{
                                 width: "100%",
                                 height: "100%",
                                 outline: "none",
                             }}
                             >
-                                {/* <div className="sub_head">
-                                    <div className="head_text">{props.data.data[state.currentIndex].content[state.contentIndex].subtitle?props.data.data[state.currentIndex].content[state.contentIndex].subtitle : ""}</div>
-                                    <div className="head_desciption">{props.data.data[state.currentIndex].content[state.contentIndex].description}</div>
-                                </div>
-                                <div className="img_container">
-                                    <img alt="day_of_img" src={props.data.data[state.currentIndex].content[state.contentIndex].image.default}></img>
-                                </div> */}
-                            <Carousel pagination={false} showArrows={true} showArrows={false} ref={slider} className="carousel">
+                                
+                            <Carousel pagination={false} showArrows={false} ref={slider} onChange={(data) =>{ console.log("data",data); setState({currentContentIndex: data.index})}} className="carousel">
                                 {props.data.data[state.currentIndex].content.map((data, index) => (
-                                    index==state.contentIndex?
-                                        <div className={slideClass} id="topic" key={index}>
-                                            <div className="sub_head">
-                                                <div className="head_text">{data.subtitle}</div>
-                                                <div className="head_desciption">{data.description}</div>
-                                            </div>
-                                            <div className="img_container">
-                                                <img alt="day_of_img" src={data.image.default}></img>
-                                            </div>
+                                    <div className="topic_content_left" id="topic" key={index}>
+                                        <div className="sub_head">
+                                            <div className="head_text">{data.subtitle}</div>
+                                            <div className="head_desciption">{data.description}</div>
                                         </div>
-                                    : null
+                                        <div className="img_container">
+                                            <img alt="day_of_img" src={data.image.default}></img>
+                                        </div>
+                                    </div>
                                 ))}
                             </Carousel>
                         </ReactScrollWheelHandler>
