429e4b847626fd5570a8b1fbfb6e2c531b0debc2
dinesh-bb <dinesh@brownbutton.io>
2021-02-25 11:45:19 +0530

    For you and your partner Screen done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Preparing_for_your_surgery from './screens/preparing_for_your_surgery/pre
 import ResumingHome from './screens/resuming_home/resuming_home.screen';
 import HomExerciseAfterSurgery from './screens/home_exercise_after_surgery/home_exercise_after_surgery';
 import ExerciseTargetForLife from './screens/exercise_target_for_life/exercise_target_for_life.screen';
+import ForYouAndYourPartner from './screens/for_you_and_your_partner/for_you_and_your_partner.screen';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route path="/returning_home" component={ReturningHome} />
           <Route path="/home_exercise_after_surgery" component={HomExerciseAfterSurgery} />
           <Route path="/exercise_target_for_life" component={ExerciseTargetForLife} />
+          <Route path="/for_you_and_your_partner" component={ForYouAndYourPartner} />
           <Route  path="/" />          
         </Switch>
       </BrowserRouter>
